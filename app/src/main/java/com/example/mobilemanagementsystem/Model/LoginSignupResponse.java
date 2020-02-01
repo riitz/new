04561467acdb7c7d616b5a448da91c0d33592bbe
riitz <riteshsht@gmail.com>
2020-02-01 19:31:23 +0545

    creating model of loginsignup activity

---
+++ app/src/main/java/com/example/mobilemanagementsystem/Model/LoginSignupResponse.java
@@ -0,0 +1,18 @@
+package com.example.mobilemanagementsystem.Model;
+
+public class LoginSignupResponse {
+
+    private boolean success;
+    private String status;
+
+
+    public boolean getSuccess() {
+        return success;
+    }
+    public String getStatus(){
+        return  status;
+    }
+
+
+
+}
