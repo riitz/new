1b0b9d4793e80f9b8343e069540be6806cbff3f7
riitz <riteshsht@gmail.com>
2020-02-07 19:26:32 +0545

    creating url

---
+++ app/src/main/java/com/example/mobilemanagementsystem/URL/Url.java
@@ -0,0 +1,28 @@
+package com.example.mobilemanagementsystem.URL;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Url {
+
+
+
+//   for PC use
+// public static final String BASE_URL ="http://10.0.2.2:3002/";
+
+    //  for mobile use
+    public static final String  BASE_URL="http://192.168.137.105:3002/";
+
+
+    public static String token = "Bearer ";
+    public static String imagePath = BASE_URL + "uploads/";
+
+
+    public static Retrofit getInstance() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
